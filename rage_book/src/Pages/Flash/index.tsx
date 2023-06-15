b2db16cc5cbc5763aa5a5156a21d214c9814f4f6
Ronan Wojdyla <ronan.wojdyla@hotmail.fr>
2023-06-15 10:13:04 +0200

    ajout des flash

--- rage_book/src/Pages/Flash/index.tsx
+++ rage_book/src/Pages/Flash/index.tsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react"
 import { FlashStyle } from "../../Styles/FlashStyle"
+import { gsap } from "gsap"
 
 
 
 function Flash(){
+
+    useEffect(() =>{
+    gsap.fromTo(".img", {opacity: 0}, {opacity:1, duration: 1, stagger: 0.06})})
+
     return(
         <FlashStyle>
             <h1 className="tittle">Flash dispo :</h1>
             <div className="grid">
-                {[... new Array(10)].map((_, i) => (
-                <img src={require('./../../Assets/Fusioné.png')} className="img"/>
+                {[... new Array(16)].map((_, i) => (
+                <img alt="flash" src={require(`./../../Assets/Flash/flash${i - 1}.jpeg`)} className="img"/>
             ))}
             </div>
         </FlashStyle>
